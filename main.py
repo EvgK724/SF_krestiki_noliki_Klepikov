cca32039d153d1a42dbc71f5c9ee10624a444c68
EvgK724 <117834576+EvgK724@users.noreply.github.com>
2022-11-12 14:00:44 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,120 @@
+field = [[" "]*3 for _ in range(3)]
+
+
+def greet():
+    print("-----------------")
+    print(" Приветствуем Вас ")
+    print("     в игре      ")
+    print(" крестики-нолики ")
+    print("-----------------")
+    print(" формат ввода: X Y ")
+    print(" X - номер строки ")
+    print(" Y - номер столбца ")
+
+
+greet()
+
+
+def show():
+    print("  0 1 2 ")
+    for i in range(len(field)):
+        print(str(i), *field[i])
+
+
+def ask():
+    while True:
+        coord = input("      Ваш ход: ").split()
+        if len(coord) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = coord
+
+        if not(x.isdigit()) or not(y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 and 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+
+
+def check_win():
+    win_coord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                 ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                 ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for coord in win_coord:
+        symbols = []
+        for c in coord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+
+
+num = 0
+while True:
+    show()
+    num += 1
+    if num % 2 == 1:
+        print(" Ходит крестик ")
+    else:
+        print(" Ходит нолик ")
+
+    x, y = ask()
+
+    if num % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+    if check_win():
+        break
+    if num == 9:
+        print(" Ничья ")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
